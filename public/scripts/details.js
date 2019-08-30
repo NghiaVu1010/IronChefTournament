21fc6ca891c57c1a900e18a183f2abdb6cadfb7a
Nghia Vu <Nghia.vu1010@gmail.com>
2019-08-30 10:13:26 -0400

    Formatting code

--- public/scripts/details.js
+++ public/scripts/details.js
@@ -6,19 +6,25 @@
 "use strict";
 
 /*
-* Insert data into table with a new row
+* Show team info by fetching and filtering into specific functions
 * 
-* @param element (Object) - Create an row element to build table
+* @param managerCard (Object) - Card holding info about manager
+* @param requirementCard (Object) - Card holding info about team
+* @param memberCard (Object) - Card holding info about members
 */
 function showData(list) {
+    // Header is set to the team name
     $("#teamName").text(list.TeamName);
 
+    // Create card for manager
     let managerCard = createManagerCard(list);
     $("#team-details").append(managerCard);
 
+    // Create card for team 'rules'
     let requirementCard = createRequirementCard(list);
     $("#team-details").append(requirementCard);
 
+    // Create card for members
     if(list.Members.length == 0) {
         $("#team-members").append($("<p>")
             .text("There are currently no team members registered!"));
@@ -26,12 +32,21 @@ function showData(list) {
     else {
         for(let i = 0; i < list.Members.length; i++) {
             let memberCard = createMemberCard(list.Members[i]);
-            
             $("#team-members").append(memberCard);
         }
     }
 }
 
+/*
+* Create a card for each member that gets passed
+* 
+* @param card (Object) - Main div holding the card together
+* @param cardHead (Object) - Card head displaying generic text
+* @param cardBody (Object) - Card body holding interested elements
+* @param cardTitle (Object) - Card title holding manager name
+* @param cardText1 (Object) - Card text holding manager phone
+* @param cardText2 (Object) - Card text holding manager email
+*/
 function createManagerCard(team) {
     // Dynamically create card info
     let card = $("<div>", {class: "card col-md-6 m-4"});
@@ -41,11 +56,11 @@ function createManagerCard(team) {
     let cardText1 = $("<p>", {text: "Phone # - " + team.ManagerPhone});
     let cardText2 = $("<p>", {text: "Email - " + team.ManagerEmail})
 
-    // Append the body
+    // Append the head/body
     card.append(cardHead)
         .append(cardBody);
 
-    // Append each tag to the body
+    // Append each element to the body
     cardBody.append(cardTitle)
         .append(cardText1)
         .append(cardText2)
@@ -53,39 +68,52 @@ function createManagerCard(team) {
     return card;
 }
 
+/*
+* Create a card for 'requirement/rules'
+* 
+* @param card (Object) - Main div holding the card together
+* @param cardHead (Object) - Card head displaying generic text
+* @param cardBody (Object) - Card body holding interested elements
+* @param cardText1 (Object) - Card text holding max size
+* @param cardText2 (Object) - Card text holding age range
+* @param cardText3 (Object) - Card text holding gender
+*/
 function createRequirementCard(team) {
     // Dynamically create card info
     let card = $("<div>", {class: "card col-md-4 m-4"});
     let cardHead = $("<h3>", {text: "Team Rules", class: "card-header text-center"})
     let cardBody = $("<div>", {class: "card-body"});
-    let cardTitle = $("<p>", {text: "\u2022 Max Team Size - " + team.MaxTeamMembers});
-    let cardText1 = $("<p>", {text: "\u2022 Ages " + team.MinMemberAge + "-" + team.MaxMemberAge});
-    let cardText2 = $("<p>", {text: "\u2022 Gender - " + team.TeamGender})
+    let cardText1 = $("<p>", {text: "\u2022 Max Team Size - " + team.MaxTeamMembers});
+    let cardText2 = $("<p>", {text: "\u2022 Ages " + team.MinMemberAge + "-" + team.MaxMemberAge});
+    let cardText3 = $("<p>", {text: "\u2022 Gender - " + team.TeamGender})
 
-    // Append the body
+    // Append the head/body
     card.append(cardHead)
         .append(cardBody);
 
-    // Append each tag to the body
-    cardBody.append(cardTitle)
-        .append(cardText1)
+    // Append each element to the body
+    cardBody.append(cardText1)
         .append(cardText2)
+        .append(cardText3)
 
     return card;
 }
 
+/*
+* Create a card for each member
+* 
+* @param card (Object) - Main div holding the card together
+* @param cardHead (Object) - Card head member name
+* @param cardBody (Object) - Card body holding interested elements
+* @param cardText1 (Object) - Card text holding age
+* @param cardText2 (Object) - Card text holding gender
+* @param cardText3 (Object) - Card text holding email
+* @param cardText4 (Object) - Card text holding phone
+* @param cardText5 (Object) - Card text holding contact name
+* @param editCard (Object) - Edit button created for that member
+* @param removeCard (Object) - Remove/delete button created for that member
+*/
 function createMemberCard(member) {
-
-    let editCard = $("<button>", {type: "button", 
-        class: "float-right", 
-        id: "editMember" + member.MemberId, 
-        html: "<i class='fas fa-pencil-alt'></i>"});
-
-    let removeCard = $("<button>", {type: "button", 
-        class: "float-right", 
-        id: "removeMember" + member.MemberId,
-        html: "<i class='fas fa-times'></i>"});
-
     // Dynamically create card info
     let card = $("<div>", {class: "card col-md-3"});
     let cardHead = $("<h5>", {text: member.MemberName, class: "card-header"})
@@ -96,10 +124,23 @@ function createMemberCard(member) {
     let cardText4 = $("<p>", {text: "Phone # - " + member.Phone})
     let cardText5 = $("<p>", {text: "Contact Person - " + member.ContactName})
 
+    // Creates a edit button to be used in a action modal
+    let editCard = $("<button>", {type: "button", 
+        class: "float-right", 
+        id: "editMember" + member.MemberId, 
+        html: "<i class='fas fa-pencil-alt'></i>"});
+
+    // Creates a remove button to be used in a action modal
+    let removeCard = $("<button>", {type: "button", 
+        class: "float-right", 
+        id: "removeMember" + member.MemberId,
+        html: "<i class='fas fa-times'></i>"});
+
     // Append the body
     card.append(cardHead)
         .append(cardBody);
 
+    // Append the buttons
     cardHead.append(removeCard)
         .append(editCard);
     
@@ -113,6 +154,11 @@ function createMemberCard(member) {
     return card;
 }
 
+/*
+* Dynamically create a modal to edit the team
+* 
+* WIP
+*/
 function createEditModal(list) {
     let form = $("<form>", {id: "editForm"});
     let newDiv = $("<div>", {class: "form-group"});
@@ -143,6 +189,55 @@ function createEditModal(list) {
     }
 }
 
+/*
+* Dynamically create a modal to edit a member
+* 
+* @param card (Object) - Main div holding the card together
+* @param cardHead (Object) - Card head member name
+* @param cardBody (Object) - Card body holding interested elements
+* @param cardText1 (Object) - Card text holding age
+* @param cardText2 (Object) - Card text holding gender
+* @param cardText3 (Object) - Card text holding email
+* @param cardText4 (Object) - Card text holding phone
+* @param cardText5 (Object) - Card text holding contact name
+* @param editCard (Object) - Edit button created for that member
+* @param removeCard (Object) - Remove/delete button created for that member
+*/
+function createEditModal(list) {
+    let form = $("<form>", {id: "editForm"});
+    let newDiv = $("<div>", {class: "form-group"});
+    let labelArr = ["Email:","Member Name:", "Contact Name:", "Age:", "Gender:", "Phone:"];
+
+    $(".modal-title").text("Edit");
+    $(".modal-body").empty()
+        .append(form);
+
+    let i = 0;
+    for(let key in list) {
+        if(key != "MemberId") {
+            newDiv = $("<div>", {class: "form-group"});
+
+            let newLabel = $("<label>", {for: "modal-" + key + "-" + list.MemberId, text: labelArr[i]});
+            let newInput = $("<input>", {type: "text", 
+                class: "form-control", 
+                id: "modal-" + key + "-" + list.MemberId, 
+                name: key.toLowerCase(), 
+                value: list[key]});
+            
+            form.append(newDiv);
+            newDiv.append(newLabel)
+                .append(newInput);
+
+            i++;
+        }
+    }
+}
+
+/*
+* Dynamically create a modal to remove a member
+* 
+* @param newP (Object) - Element to confirm if the user wants to delete
+*/
 function createRemoveModal(list) {
     let newP = $("<p>", {text: `Are you sure you want to delete ${list.MemberName} from this team?`});
 
@@ -151,6 +246,33 @@ function createRemoveModal(list) {
         .append(newP);
 }
 
+/*
+* AJAX call to edit team info
+*
+* WIP to be used with edit team modal
+*/
+function editTeam(teamId, memberId) {
+    $.ajax({
+        type: "PUT",
+        url: `/api/teams`,
+        data: serial
+        })
+        .done(function() {
+            alert("Edit successful!");
+            location.reload();
+        })
+        .fail(function() {
+            alert("There was a problem, please try again.");
+        });
+
+    return false;
+}
+
+/*
+* AJAX call to edit a team member
+*
+* @param serial (String) - Concatenated serial form
+*/
 function editMember(teamId, memberId) {
     let serial = `teamid=${teamId}&memberid=${memberId}&` + $("#editForm").serialize();
 
@@ -170,6 +292,9 @@ function editMember(teamId, memberId) {
     return false;
 }
 
+/*
+* AJAX call to delete a member from the team
+*/
 function removeMember(teamId, memberId) {
     $.ajax({
         type: "DELETE",
@@ -190,14 +315,17 @@ $(function() {
     let urlParams = new URLSearchParams(location.search);
     let teamId = urlParams.get("teamId");
 
-    //grab course data and show
+    // Holds all team info
     let objs;
+
+    // Holds current member id that has been selected
     let currMemberId;
+
+    // Holds a string of the action to be done with modal
     let action;
 
     $.getJSON("/api/teams/" + teamId, (data) => {
         objs = data;
-
         showData(objs)
 
         for(let i = 0; i < objs.Members.length; i++) {
@@ -220,16 +348,17 @@ $(function() {
 
     });
 
-    //link to registration for current class
+    // Link to add a new team member
     $("#addMemberBtn").on("click", function() {
         location.href = "add_member.html?teamId=" + teamId;
     });
 
-    //cancel back to courses
+    // Cancel back to teams
     $("#cancelBtn").on("click", function() {
         location.href = "teams.html";
     });
 
+    // Confirm modal button switches based on what action is being done with modal
     $("#confirmModalBtn").on("click", function () {
         switch(action) {
             case 'edit':
